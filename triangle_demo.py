5f8ff559f6fe3e924c31df44c9f1246f0354bd56
florije1988 <florije1988@gmail.com>
2015-04-20 18:05:09 +0800

    nothing

--- triangle_demo.py
+++ triangle_demo.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_triangle import Triangle
 
 app = Flask(__name__)
+Bootstrap(app)
+Triangle(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
